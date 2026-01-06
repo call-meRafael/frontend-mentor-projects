6ca9e8e7b810510216991e155de58a99d886a404
countdown-br <raaphael.araujo@gmail.com>
2026-01-06 11:35:35 -0300

    card-component: adjusting the logic of the clicks

--- article-component/app.js
+++ article-component/app.js
@@ -1,20 +1,45 @@
 const shareBtn = document.querySelector('.share-btn');
 const modalShareLinks = document.querySelector('.share-popup');
 
-const openModal = function () {
-    modalShareLinks.classList.remove('hidden');
-    shareBtn.style.backgroundColor = 'var(--light-grayish-blue)';
-    shareBtn.style.color = 'var(--dark-gray-blue)';
+const clickedOutside = function (e) {
+    if (!modalShareLinks.contains(e.target)) {
+        closeModal();
+    }
+};
+
+const toggleModal = function () {
+    const hiddenModal = modalShareLinks.classList.contains('hidden');
+    modalShareLinks.classList.toggle('hidden');
+    
+    if (hiddenModal) {
+        shareBtn.style.backgroundColor = 'var(--light-grayish-blue)';
+        shareBtn.style.color = 'var(--dark-gray-blue)';
+
+        setTimeout(function () {
+            document.addEventListener('click', clickedOutside);
+        }, 0);
+
+    } else {
+        shareBtn.style.backgroundColor = '';
+        shareBtn.style.color = '';
+        document.removeEventListener('click', clickedOutside);
+    }
 }
 
 const closeModal = function () {
     modalShareLinks.classList.add('hidden');
+    shareBtn.style.backgroundColor = '';
+    shareBtn.style.color = '';
+    document.removeEventListener('click', clickedOutside);
 }
 
-shareBtn.addEventListener('click', openModal);
+shareBtn.addEventListener('click', toggleModal);
 
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modalShareLinks.classList.contains('hidden')) {
         closeModal();
     }
 });
+
+
+
