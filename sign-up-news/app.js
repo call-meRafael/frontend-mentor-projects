d776a5e3cadeaf52ee35b1598bcdb8dcbada5175
countdown-br <raaphael.araujo@gmail.com>
2026-01-19 14:36:22 -0300

    sign-up-project: styles done 80% logical test through JS started

---
+++ sign-up-news/app.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('.--form-content');
+const cardContainer = document.querySelector('.card-container');
+const emailInput = document.querySelector('.email-input');
+const errorMsg = document.querySelector('.alert-msg');
+
+
+
