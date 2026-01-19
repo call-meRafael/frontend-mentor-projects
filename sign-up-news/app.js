49c7f7d6d62ee934ebb09d25991889e5d3427f16
countdown-br <raaphael.araujo@gmail.com>
2026-01-19 16:42:31 -0300

    logic-validate: adding first set of logics

--- sign-up-news/app.js
+++ sign-up-news/app.js
@@ -5,3 +5,24 @@ const errorMsg = document.querySelector('.alert-msg');
 
 
 
+function emailAproved(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    return emailRegex.test(email);
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = emailInput.value.trim();
+    if (!email) {
+        errorMsg.classList.remove('hidden');
+    }
+
+
+    if (!emailAproved(email)) {
+        errorMsg.classList.remove('hidden');
+        emailInput.classList.add('error-state');
+        emailInput.style.borderColor = 'var(--prim-red)';
+    }
+
+})
