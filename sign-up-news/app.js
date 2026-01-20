4ed02e982cebdde7dd5c5a2436fbd9f5e157a3e8
countdown-br <raaphael.araujo@gmail.com>
2026-01-20 10:37:58 -0300

    sign-up-project: finishing  validation and swap layout for success.

--- sign-up-news/app.js
+++ sign-up-news/app.js
@@ -2,6 +2,11 @@ const form = document.querySelector('.--form-content');
 const cardContainer = document.querySelector('.card-container');
 const emailInput = document.querySelector('.email-input');
 const errorMsg = document.querySelector('.alert-msg');
+const successCard = document.querySelector('.success-card');
+const emailSuccess = document.querySelector('.email-success');
+
+const submitBtn = document.getElementById('btn-submit');
+const dismissBtn = document.getElementById('dismiss-btn');
 
 
 
@@ -12,10 +17,12 @@ function emailAproved(email) {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    
 
     const email = emailInput.value.trim();
     if (!email) {
         errorMsg.classList.remove('hidden');
+        return;
     }
 
 
@@ -23,6 +30,30 @@ form.addEventListener('submit', (e) => {
         errorMsg.classList.remove('hidden');
         emailInput.classList.add('error-state');
         emailInput.style.borderColor = 'var(--prim-red)';
-    }
+        return;
+    } 
+
+    emailSuccess.textContent = email;
+    successCard.classList.remove('hidden');
 
 })
+
+dismissBtn.addEventListener('click', function () {
+    successCard.classList.add('hidden');
+    form.reset();
+})
+
+emailInput.addEventListener('input', function () {
+    const email = emailInput.value.trim();
+
+    if (email && !emailAproved(email)) {
+        errorMsg.classList.remove('hidden');
+        emailInput.classList.add('error-state');
+        emailInput.style.borderColor = 'var(--prim-red)';
+        return;
+    } else {
+        errorMsg.classList.add('hidden');
+        emailInput.classList.remove('error-state');
+        emailInput.style.borderColor = 'var(--gray)';
+    }
+});
